6477bd808416b1e312b1620407385e5804639337
Jon Oliver <jon@joliver.net>
2014-04-03 10:34:09 -0400

    Add rspec to tool installation

--- script/tool_installer.sh
+++ script/tool_installer.sh
@@ -4,7 +4,7 @@ source $(dirname $0)/tool_installer_functions.sh
 echo "Installing tools/dependencies"
 
 install_ruby
-install_rubygems "rails"
+install_rubygems "rails" "rspec"
 install_homebrew "git" "wget" "tree" "ack" "tmux" "node"
 install_npm_global "grunt-cli" "yo" "coffee-script" "mongodb" "mocha" "express"
 
