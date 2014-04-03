d42c45c722ceb3b8dc9d6bc96280e5d8c123d05e
Jon Oliver <jon@joliver.net>
2014-04-03 12:08:00 -0400

    Added installer for RubyTest for Sublime

---
+++ script/RubyTest/install_RubyTest.sh
@@ -0,0 +1,3 @@
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+#git clone https://github.com/maltize/sublime-text-2-ruby-tests.git RubyTest
+cp ~/dotfiles/script/RubyTest/RubyTest.sublime-settings User
