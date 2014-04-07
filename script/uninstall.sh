667b675ca927b433bb0a0d6ecdd2d5679c2c5369
Jon Oliver <jon@joliver.net>
2014-04-07 15:12:45 -0400

    Update uninstall

--- script/uninstall.sh
+++ script/uninstall.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
-#undo everything done in install.sh
-#rm -rf $HOME/.dotfiles
-#rm $HOME/zshrc
-#rm $HOME/.ssh/config
 
-# Temporary "uninstall" for testing
-cd $HOME
+rm -rf $HOME/dotfiles
+rm -rf $HOME/.oh-my-zsh
+rm .zshrc
 
-backup=true
-
-if [ $backup = true ]; then
-
-  mv .oh-my-zsh .oh-my-zsh.bak
-  mv dotfiles dotfiles.bak
-  mv .rvm .rvm.bak
-else
-  rm -rf dotfiles
-  rm -rf .oh-my-zsh
-  rm -rf .rvm
+if [ -f "$HOME/.zshrc.bak" ]; then
+  mv $HOME/.zshrc.bak $HOME/.zshrc
 fi
 
-rm .zshrc
 chsh -s /bin/bash
